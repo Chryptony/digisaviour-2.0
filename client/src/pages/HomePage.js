4e413fced9e26e8a3575c8c0341efcbafa4a3cb0
Chryptony willie <chryptony@gmail.com>
2024-09-11 13:48:45 +0700

    hampir beres

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,8 @@
-import React,{ useEffect } from 'react'
+import React, { useEffect } from 'react'
 import '../App.js'
 import { Link } from 'react-router-dom'
+import Ten from "../images/10.jpg";
+
 const HomePage = () => {
   useEffect(() => {
     window.scrollTo(0, 0); // Melakukan scroll ke atas saat komponen dimuat
@@ -9,19 +11,28 @@ const HomePage = () => {
     <div>
       <div className="container-fluid text-start homescreen">
         <div class="container-fluid a2">
+          <h3 className="text-start">Cooling tower anda bermasalah ?
+          </h3>
+          <h3 className="text-start fw-bold">Ada kerak di fasilitas Cooling Tower ?</h3>
+          <h5 className="text-start">mau coba beralih dari bahan kimia untuk mengatasi scaling problem ?</h5>
+        </div>
+      </div>
+      <div className="container-fluid homescreen3">
+        <div class="container text-center">
+          <div class="row align-items-center">
+            <div class="col">
+              <img src={Ten}/>
+            </div>
+            <div class="col">
+            <div class="container-fluid a2">
           <h3 className="text-start">Tidak perlu bahan kimia untuk mencegah dan mengatasi</h3>
           <h3 className="text-start fw-bold">kerak di fasilitas Cooling Tower</h3>
           <h5 className="text-start t1 py-5"> Mau tau cara praktis dan ekonomis untuk mengatasi scaling problem ?</h5>
         </div>
-        <Link className=" link-underline link-underline-opacity-0 text btn btn-success fw-bold a2" to="/Produk-Digisaviour"> MAU TAU DONG</Link>
-      </div>
-      <div className="container-fluid text-center homescreen2">
-      <div class="container-fluid">
-          <h3 className="text-center">Anda memiliki masalah kerak( scaling ) di Heat Exchanger dan Cooling Tower</h3>
-          <h3 className="text-center fw-bold">atau mau konsultasi gratis dengan tim ahli kami ?</h3>
-          <p className="text-center py-5"> klik link dibawah ini</p>
+        <Link className=" link-underline link-underline-opacity-0 text btn btn-success btn-lg fw-bold a2" to="/Produk-Digisaviour"> MAU TAU DONG</Link>
+            </div>
+          </div>
         </div>
-        <Link className="link-underline link-underline-opacity-0 text btn btn-success fw-bold" to="/konsultasi">Konsultasi Gratis</Link>
       </div>
     </div>
   )
